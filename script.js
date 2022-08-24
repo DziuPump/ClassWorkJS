d8c1abf74b2f4f6db19f2317ac9a03e31737097d
DziuPump <dziugaspumputis@example.com>
2022-08-25 00:11:02 +0300

    added task #1

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-//3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
+//1. Write a function that takes two numbers (a and b) as Sum a and Return the result
+const sum = function(a, b){
+    return a+b;
+}
 
-const a = 123456789
+
+
+
+
+//3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
 
 const splitter = function (a){
     const array = Array.from(String(a), Number);
     return array;
 }
 
-console.log(splitter(a))
